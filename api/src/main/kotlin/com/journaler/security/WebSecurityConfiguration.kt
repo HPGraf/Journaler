241d49437537b3fe64e2080e5446d053f2c7b522
HPGraf <hans-peter.graf@outlook.com>
2018-10-04 21:03:04 +0200

    added some tests

--- api/src/main/kotlin/com/journaler/security/WebSecurityConfiguration.kt
+++ api/src/main/kotlin/com/journaler/security/WebSecurityConfiguration.kt
@@ -1,6 +1,6 @@
 package com.journaler.security
 
-import com.journaler.api.service.UserService
+import com.journaler.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
